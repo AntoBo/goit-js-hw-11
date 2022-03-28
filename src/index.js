f09bff35a9884b662a8e179a000c8dae0b562c72
AntoBo <anton.bondarenkooooo@gmail.com>
2022-03-28 16:24:13 +0300

    axios ok

--- src/index.js
+++ src/index.js
@@ -1,7 +1,16 @@
 import './sass/main.scss';
 import axios from 'axios';
 
+const options = {
+  params: {
+    key: '26342671-26c26899736dd731f47ba4106',
+    q: 'cat',
+    per_page: 5,
+  },
+};
+axios.defaults.baseURL = 'https://pixabay.com/api/';
+
 axios
-  .get('https://pokeapi.co/api/v2/pokemon/ditto')
-  .then(r => r.json())
-  .then(console.log);
+  .get('', options)
+  .then(r => console.log(r.data))
+  .catch(err => console.log('error: ', err));
